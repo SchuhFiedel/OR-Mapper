23ea68dc250791be3f715bd18e7a071fa6ba06cc
Maximiliano Zulli <if19b183@dev.azure.com>
2022-01-09 17:15:00 +0100

    2 Unit Tests

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -1,4 +1,8 @@
 using NUnit.Framework;
+using SWE3_Zulli.OR.Framework;
+using SWE3_Zulli.OR.Framework.Cache;
+using SWE3_Zulli.OR.Models;
+using System;
 
 namespace TestProject
 {
@@ -15,16 +19,44 @@ namespace TestProject
 
     public class Tests
     {
+        private int _stIDCount = 0;
+        private int _tcIDCount = 0;
+        private int _courseIDCount = 0;
+        private int _classIDCount = 0;
+
         [SetUp]
         public void Setup()
         {
-
+            ORMapper.ConnectionString = "Host=localhost;Username=postgres;Password=postgres;Database=ORTest;Pooling=true;Include Error Detail=true";
+            ORMapper.Cache = new TrackerCache();
+            //ORMapper.ClearDB();
         }
 
-        [Test]
-        public void Test1()
+        [Test, Order(0)]
+        public void InsertStudent()
         {
-            Assert.Pass();
+            Student s = new Student()
+            {
+                FirstName = "Bongo",
+                LastName = "Dogo",
+                BirthDate = DateTime.Today,
+                ID = _stIDCount,
+                Gender = Gender.OTHER,
+                Grade = 15
+            };
+            ORMapper.Save(s);
         }
+
+        [Test, Order(1)]
+        public void GetStudent()
+        {
+            Student s = ORMapper.Get<Student>(_stIDCount);
+            Assert.AreEqual("Bongo", s.FirstName);
+            Assert.AreEqual("Dogo", s.LastName);
+            Assert.AreEqual(DateTime.Today, s.BirthDate);
+            Assert.AreEqual(Gender.OTHER, s.Gender);
+            Assert.AreEqual(15, s.Grade);
+        }
+
     }
 }
